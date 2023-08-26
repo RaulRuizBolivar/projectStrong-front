0d3284a7deaa7032dd312033c9777a815c65a4ce
Raul <raul.ruiz@wyred.biz>
2023-08-26 11:25:44 +0200

    git init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'strong-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'projectStrong';
-}
+export class AppComponent {}
