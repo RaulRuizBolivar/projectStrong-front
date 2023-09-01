ab9cc3ce103d4325bbd0befd6f346d28c0b5b8f7
Raul <raul.ruiz@wyred.biz>
2023-09-01 20:30:35 +0200

    development - HOTFIX - remove mockups

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,28 +1,8 @@
 import { Component } from '@angular/core';
-import { WorkOut } from 'src/app/interfaces/work-out';
 
 @Component({
   selector: 'strong-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent {
-  workOuts: WorkOut[] = [
-    {
-      title: 'titulo',
-      description: '',
-      exercices: [],
-      duration: 12,
-      records: 2,
-      volume: 33,
-      comments: [],
-      date: new Date(Date.now()),
-      creator: {
-        name: 'nombre',
-        username: 'usuarIo',
-        profilePicture: 'url',
-      },
-      likedBy: [],
-    },
-  ];
-}
+export class FeedComponent {}
